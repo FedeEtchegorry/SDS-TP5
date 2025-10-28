ac3ac2659988e70764b1d0ca603a51c146ba357e
Máximo Chiatellino <mchiatellino@itba.edu.ar>
2025-10-28 16:28:05 -0300

    "Add Social Force Model (SFM) implementation to calculate pedestrian interaction forces"

---
+++ PedestrianDynamic/src/SFM.java
@@ -0,0 +1,58 @@
+import static java.lang.Math.*;
+
+public class SFM {
+
+    private static final double kn = 1.2e5;   // [N/m] fuerza normal (contacto)
+    private static final double kt = 2.4e5;   // [kg/m/s] fricción tangencial
+    private static final double tau = 0.5;    // [s] tiempo de relajación
+    private static final double vd = 1.7;     // [m/s] velocidad deseada
+
+    public static double[] contactForce(Particle i, Particle j) {
+        double dx = j.x() - i.x();
+        double dy = j.y() - i.y();
+        double dij = hypot(dx, dy);
+        if (dij == 0) return new double[]{0, 0};
+
+        double nx = dx / dij;
+        double ny = dy / dij;
+        double tx = -ny;
+        double ty = nx;
+
+        double overlap = i.radius() + j.radius() - dij;
+        if (overlap <= 0) return new double[]{0, 0}; // no hay contacto
+
+        double g = overlap; // g(x) = max(0, x)
+
+        // Velocidades
+        double vix = cos(i.angle()) * i.speed();
+        double viy = sin(i.angle()) * i.speed();
+        double vjx = cos(j.angle()) * j.speed();
+        double vjy = sin(j.angle()) * j.speed();
+
+        double dvx = vjx - vix;
+        double dvy = vjy - viy;
+        double deltaVt = dvx * tx + dvy * ty;
+
+        // Fuerzas normal y tangencial
+        double fn = kn * g;
+        double ft = kt * g * deltaVt;
+
+        double fx = fn * nx + ft * tx;
+        double fy = fn * ny + ft * ty;
+
+        return new double[]{fx, fy};
+    }
+
+    public static double[] drivingForce(Particle i, double targetAngle) {
+        double desiredVx = vd * cos(targetAngle);
+        double desiredVy = vd * sin(targetAngle);
+
+        double vix = cos(i.angle()) * i.speed();
+        double viy = sin(i.angle()) * i.speed();
+
+        double fx = i.getMass() * ((desiredVx - vix) / tau);
+        double fy = i.getMass() * ((desiredVy - viy) / tau);
+
+        return new double[]{fx, fy};
+    }
+}
